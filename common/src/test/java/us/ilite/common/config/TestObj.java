a1274f1194739868397c6faab3f8eecb252cb985
Chris O'Connell <coconnell84@gmail.com>
2020-01-09 18:46:15 -0500

    Took out unit tests that are broken and no longer valid

--- common/src/test/java/us/ilite/common/config/TestObj.java
+++
@@ -1,9 +0,0 @@
-package us.ilite.common.config;
-/**
- * Add your docs here.
- */
-public class TestObj {
-    public static double kControlLoopPeriod = 500; // seconds
-
-    public static double kNetworkTableUpdateRate = 500;
-}
