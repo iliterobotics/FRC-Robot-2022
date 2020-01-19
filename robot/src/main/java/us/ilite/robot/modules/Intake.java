5dd4c7a4d9f767e190e064e972b9a243479f40b5
Agrim Sharma <76flyershd@gmail.com>
2020-01-19 10:52:21 -0500

    enum added

--- robot/src/main/java/us/ilite/robot/modules/Intake.java
+++ robot/src/main/java/us/ilite/robot/modules/Intake.java
@@ -1,9 +1,15 @@
 package us.ilite.robot.modules;
 
 public class Intake extends Module {
+
+    public enum EIntakeState{
+        INTAKE,
+        STOP,
+        REVERSE;
+    }
     @Override
     public void modeInit(double pNow) {
-        
+
     }
 
     @Override
