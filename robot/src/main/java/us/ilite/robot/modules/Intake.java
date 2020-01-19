0e5537fe433247b8e54581464a8795483179231d
Agrim Sharma <76flyershd@gmail.com>
2020-01-19 11:02:46 -0500

    Motors added as well as constructor

--- robot/src/main/java/us/ilite/robot/modules/Intake.java
+++ robot/src/main/java/us/ilite/robot/modules/Intake.java
@@ -1,12 +1,33 @@
 package us.ilite.robot.modules;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.flybotix.hfr.util.log.ILog;
+import com.flybotix.hfr.util.log.Logger;
+import com.revrobotics.CANSparkMax;
+import us.ilite.common.Data;
+
 public class Intake extends Module {
+
+    private CANSparkMax mIntakeMotor;
+    private TalonSRX mTalonOne;
+    private TalonSRX mTalonTwo;
+    private TalonSRX mTalonThree;
+
+    private Data mData;
+
+    private ILog mLog = Logger.createLog(this.getClass());
+
 
     public enum EIntakeState{
         INTAKE,
         STOP,
         REVERSE;
     }
+
+    public Intake( Data pData ) {
+        this.mData = pData;
+    }
+
     @Override
     public void modeInit(double pNow) {
 
