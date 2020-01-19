8cdd892620cd88f73178d863702ad1d920b5617f
Agrim Sharma <76flyershd@gmail.com>
2020-01-19 10:50:05 -0500

    Initial intake commit

---
+++ robot/src/main/java/us/ilite/robot/modules/Intake.java
@@ -0,0 +1,23 @@
+package us.ilite.robot.modules;
+
+public class Intake extends Module {
+    @Override
+    public void modeInit(double pNow) {
+        
+    }
+
+    @Override
+    public void periodicInput(double pNow) {
+
+    }
+
+    @Override
+    public void update(double pNow) {
+
+    }
+
+    @Override
+    public void shutdown(double pNow) {
+
+    }
+}
