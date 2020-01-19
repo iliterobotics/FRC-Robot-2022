04a852f8c829f61fc19a4f76c5eb722140b51025
Agrim Sharma <76flyershd@gmail.com>
2020-01-19 11:13:49 -0500

    Intake power added

--- robot/src/main/java/us/ilite/robot/modules/Intake.java
+++ robot/src/main/java/us/ilite/robot/modules/Intake.java
@@ -1,19 +1,30 @@
 package us.ilite.robot.modules;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
 import com.revrobotics.CANSparkMax;
 import us.ilite.common.Data;
+import us.ilite.common.config.Settings;
+import us.ilite.robot.hardware.DigitalBeamSensor;
 
 public class Intake extends Module {
 
+    // Intake Motors
     private CANSparkMax mIntakeMotor;
     private TalonSRX mTalonOne;
     private TalonSRX mTalonTwo;
     private TalonSRX mTalonThree;
 
+    //Beam Breakers
+    private DigitalBeamSensor mBeamBreaker1;
+    private DigitalBeamSensor mBeamBreaker2;
+    private DigitalBeamSensor mBeamBreaker3;
+
     private Data mData;
+
+    private EIntakeState mIntakeState;
 
     private ILog mLog = Logger.createLog(this.getClass());
 
@@ -40,6 +51,25 @@ public class Intake extends Module {
 
     @Override
     public void update(double pNow) {
+        switch (mIntakeState) {
+            case INTAKE:
+                mIntakeMotor.set(1.0);
+                mTalonOne.set(ControlMode.PercentOutput, Settings.kIntakeTalonPower);
+                mTalonTwo.set(ControlMode.PercentOutput, Settings.kIntakeTalonPower);
+                mTalonThree.set(ControlMode.PercentOutput, Settings.kIntakeTalonPower);
+
+                break;
+            case REVERSE:
+                mTalonOne.set(ControlMode.PercentOutput, -Settings.kIntakeTalonPower);
+                mTalonTwo.set(ControlMode.PercentOutput, -Settings.kIntakeTalonPower);
+                mTalonThree.set(ControlMode.PercentOutput, -Settings.kIntakeTalonPower);
+                break;
+            case STOP:
+                mTalonOne.set(ControlMode.PercentOutput, 0d);
+                mTalonTwo.set(ControlMode.PercentOutput, 0d);
+                mTalonThree.set(ControlMode.PercentOutput, 0d);
+                break;
+        }
 
     }
 
