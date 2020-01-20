cca5e09f7392a41e7a0fbf4c6f20532bbbf45fa3
Agrim Sharma <76flyershd@gmail.com>
2020-01-20 15:03:03 -0500

    Migrated shooter to flywheel-module

--- robot/src/main/java/us/ilite/robot/modules/FlywheelModule.java
+++ robot/src/main/java/us/ilite/robot/modules/FlywheelModule.java
@@ -1,13 +1,95 @@
 package us.ilite.robot.modules;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.sensors.PigeonIMU;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.ControlType;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import us.ilite.common.Data;
+import us.ilite.common.config.Settings;
+import us.ilite.common.lib.control.PIDController;
+import us.ilite.common.types.EMatchMode;
+import us.ilite.common.types.ETargetingData;
+import us.ilite.robot.hardware.SparkMaxFactory;
+
 public class FlywheelModule extends Module {
-    @Override
-    public void readInputs(double pNow) {
+
+    private Data mData;
+    private CANSparkMax mShooter;
+    private Servo mAngler;
+    private TalonSRX mTurret;
+    private CANSparkMax mAccelerator;
+    private EShooterState mShooterState;
+    private Limelight mLimelight;
+
+    private double mDesiredOutput;
+    private double distanceToTarget;
+
+    public FlywheelModule(Data pData) {
+    //        mShooter = SparkMaxFactory.createDefaultSparkMax(Settings.Shooter.kShooterID, CANSparkMaxLowLevel.MotorType.kBrushless);
+//        mAccelerator = SparkMaxFactory.createDefaultSparkMax(Settings.Shooter.kAccelerator, CANSparkMaxLowLevel.MotorType.kBrushless);
+//        mAngler = new Servo(Settings.Shooter.kAnglerID);
+//        mTurret = new TalonSRX(Settings.Shooter.kTurretID);
+        mData = pData;
+
+
+//        mShooter.getPIDController().setP(Settings.Shooter.kShooterPGain);
+//        mShooter.getPIDController().setFF(Settings.Shooter.kShooterFF);
+//        mShooter.getPIDController().setReference(mDesiredOutput, ControlType.kVelocity);
+    }
+
+    public enum EShooterState {
+        SHOOT,
+        STOP
+    }
+
+ //   public boolean isMaxVelocity() { return mShooter.getEncoder().getVelocity() >= Settings.Shooter.kMaxShooterVelocity; }
+
+    public void setShooterState( EShooterState pShooterState ) { mShooterState = pShooterState; }
+
+    public EShooterState getShooterState() { return mShooterState; }
+
+    public void shoot() { mShooter.set(mDesiredOutput); }
+
+//    public void feed() { mAccelerator.set(Settings.Shooter.kMaxAcceleratorVelocity); }
+//
+    private void angle() {
+        if ( mData.limelight.get(ETargetingData.ty) != null ) {
+            distanceToTarget = mLimelight.calcTargetDistance(62); // inner port is 98.25
+            mAngler.set(distanceToTarget / 15); // Under Review
+        }
+        else {
+            //mAngler.setAngle(Settings.Shooter.kBaseAngle);
+        }
+    }
+
+    public void turnTurret() {
 
     }
 
     @Override
-    public void setOutputs(double pNow) {
+    public void modeInit(EMatchMode pMode, double pNow) {
+        setShooterState(EShooterState.STOP);
+    }
 
+    @Override
+    public void readInputs(double pNow) {
+//        mData.flywheel.set(EFlywheelSubsystem.CURRENT_FLYWHEEL_STATE, (double) getShooterState().ordinal());
+//        mData.flywheel.set(EFlywheelSubsystem.TARGET_FLYWHEEL_STATE, (double) mShooterState.ordinal());
+//        mData.flywheel.set(EFlywheelSubsystem.CURRENT_FLYWHEEL_VELOCITY, mShooter.getEncoder().getVelocity());
+//        mData.flywheel.set(EFlywheelSubsystem.TARGET_FLYWHEEL_VELOCITY, mDesiredOutput);
+//        mData.flywheel.set(EFlywheelSubsystem.FLYWHEEL_CURRENT, mShooter.getOutputCurrent());
+    }
+
+    @Override
+    public void setOutputs(double pNow) {
+        SmartDashboard.putNumber("Hood Angle", mAngler.getAngle());
+    }
+
+    @Override
+    public void shutdown(double pNow) {
+        setShooterState(EShooterState.STOP);
     }
 }
