dcaed59da9772d424bd55534129c01fc82e9a16b
Agrim Sharma <76flyershd@gmail.com>
2020-01-27 18:33:48 -0500

    new ground tracking module written

---
+++ robot/src/main/java/us/ilite/robot/modules/GroundTracking.java
@@ -0,0 +1,78 @@
+package us.ilite.robot.modules;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.geometry.Translation2d;
+import us.ilite.common.types.ELimelightData;
+import us.ilite.common.types.ERawTargetingData;
+import us.ilite.common.types.ETrackingType;
+import us.ilite.robot.Robot;
+
+import java.util.Optional;
+
+public class GroundTracking extends Module {
+    private final NetworkTable mTable = NetworkTableInstance.getDefault().getTable("limelight");
+    private ETrackingType mTrackingType;
+    private Limelight mLimelight;
+    @Override
+    public void readInputs(double pNow) {
+
+    }
+
+    @Override
+    public void setOutputs(double pNow) {
+        Robot.DATA.rawLimelight.reset();
+        boolean targetValid = mTable.getEntry("tv").getDouble((Double.NaN)) > 0.0;
+        Robot.DATA.rawLimelight.set(ERawTargetingData.tv, targetValid ? 1.0 : null);
+        Robot.DATA.limelight.get(ETrackingType.values()[Robot.DATA.limelight.get(ELimelightData.TRACKING_TYPE).intValue()].ordinal());
+        if (targetValid) {
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tx, mTable.getEntry("tx").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tx0, mTable.getEntry("tx0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tx1, mTable.getEntry("tx1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tx2, mTable.getEntry("tx2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ty, mTable.getEntry("ty").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ty0, mTable.getEntry("ty0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ty1, mTable.getEntry("ty1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ty2, mTable.getEntry("ty2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ta, mTable.getEntry("ta").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ta0, mTable.getEntry("ta0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ta1, mTable.getEntry("ta1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ta2, mTable.getEntry("ta2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ts, mTable.getEntry("ts").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ts0, mTable.getEntry("ts0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ts1, mTable.getEntry("ts1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.ts2, mTable.getEntry("ts2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tl, mTable.getEntry("tl").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tshort, mTable.getEntry("tshort").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tshort0, mTable.getEntry("tshort0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tshort1, mTable.getEntry("tshort1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tshort2, mTable.getEntry("tshort2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tlong, mTable.getEntry("tlong").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tlong0, mTable.getEntry("tlong0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tlong1, mTable.getEntry("tlong1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tlong2, mTable.getEntry("tlong2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.thoriz, mTable.getEntry("thor").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.thoriz0, mTable.getEntry("thor0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.thoriz1, mTable.getEntry("thor1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.thoriz2, mTable.getEntry("thor2").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tvert, mTable.getEntry("tvert").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tvert0, mTable.getEntry("tvert0").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tvert1, mTable.getEntry("tvert1").getDouble(Double.NaN));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.tvert2, mTable.getEntry("tvert2").getDouble(Double.NaN));
+
+            Robot.DATA.rawLimelight.set(ERawTargetingData.targetOrdinal, Robot.DATA.limelight.get(ELimelightData.targetOrdinal));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.calcDistToTarget, Robot.DATA.limelight.get(ELimelightData.calcDistToTarget));
+            Robot.DATA.rawLimelight.set(ERawTargetingData.calcAngleToTarget, Robot.DATA.limelight.get(ELimelightData.calcAngleToTarget));
+
+            if (Robot.DATA.limelight.get(ELimelightData.TRACKING_TYPE) != null) {
+             //   Commented out for now until issue with enums isnt fixed
+            //    Optional<Translation2d> p = Robot.DATA.limelight.get((Robot.DATA.limelight.get(ELimelightData.TRACKING_TYPE));
+          //      if (p.isPresent()) {
+                    // Robot.DATA.rawLimelight.set(ERawTargetingData.calcTargetX, p.get().x());
+                    // Robot.DATA.rawLimelight.set(ERawTargetingData.calcTargetY, p.get().y());
+                }
+            }
+        }
+
+    }
+
