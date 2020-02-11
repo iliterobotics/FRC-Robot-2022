53084e17b69965841fc4ac9fd61c53c871fe2131
Gleb Babahov <gl.babahov@gmail.com>
2020-02-10 23:08:48 -0500

    small changes to targetlock

--- robot/src/main/java/us/ilite/robot/commands/TargetLock.java
+++ robot/src/main/java/us/ilite/robot/commands/TargetLock.java
@@ -18,42 +18,22 @@ public class TargetLock implements ICommand {
     private static final double kTURN_POWER = 0.2;
     private static final int kAlignCount = 10;
     private static final double kTargetAreaScalar = 1.0;
-
-//    private DriveModule mDrive;
-//    private ITargetDataProvider mCamera;
-//    // Different throttle providers give us some control over behavior in autonomous
-//    private IThrottleProvider mTargetSearchThrottleProvider, mTargetLockThrottleProvider;
-//    private IFieldComponent mTrackingType;
-
-    private double mAllowableError, mPreviousTime, mOutput = 0.0;
+    private static final double kAllowableTargetLockError = 0.1;
 
     private boolean mEndOnAlignment = true;
-    private int mAlignedCount = 0;
-    private boolean mHasAcquiredTarget = false;
-    private boolean mStopWhenTargetLost = true;
+    private int mAlignedCount;
+    private boolean mHasAcquiredTarget;
 
-//    public TargetLock(double pAllowableError, IFieldComponent pTrackingType, ITargetDataProvider pCamera, IThrottleProvider pThrottleProvider) {
-//        this(pAllowableError, pTrackingType, pCamera, pThrottleProvider, true);
-//    }
-
-    public TargetLock(/*double pAllowableError, IFieldComponent pTrackingType, ITargetDataProvider pCamera, IThrottleProvider pThrottleProvider, boolean pEndOnAlignment*/) {
-//        this.mAllowableError = pAllowableError;
-//        this.mCamera = pCamera;
-//        this.mTargetSearchThrottleProvider = pThrottleProvider;
-//        this.mTargetLockThrottleProvider = pThrottleProvider;
-//        this.mEndOnAlignment = pEndOnAlignment;
+    public TargetLock() {
     }
 
     @Override
     public void init(double pNow) {
-
         mHasAcquiredTarget = false;
         mAlignedCount = 0;
 
         Robot.DATA.drivetrain.set(EDriveData.DESIRED_STATE, EDriveState.TARGET_ANGLE_LOCK);
         Robot.DATA.drivetrain.set(EDriveData.DESIRED_THROTTLE_PCT, 0.0);
-
-        this.mPreviousTime = pNow;
     }
 
     @Override
@@ -62,24 +42,20 @@ public class TargetLock implements ICommand {
 
         Robot.DATA.drivetrain.set(EDriveData.DESIRED_THROTTLE_PCT, Robot.DATA.operatorinput.get(InputMap.DRIVER.THROTTLE_AXIS) * Settings.Input.kSnailModePercentThrottleReduction);
 
-        if(currentData.isSet(TV) && currentData.isSet(TX)) {
+        if (currentData.isSet(TV) && currentData.isSet(TX)) {
             mHasAcquiredTarget = true;
-
             mAlignedCount++;
-            if(mEndOnAlignment && Math.abs(currentData.get(TX)) < mAllowableError && mAlignedCount > kAlignCount) {
-                // Zero drivetrain outputs in shutdown()
+            if (mEndOnAlignment && Math.abs(currentData.get(TX)) < kAllowableTargetLockError && mAlignedCount > kAlignCount) {
                 return true;
             }
 
-        // If we've already seen the target and lose tracking, exit.
-        } else if(mHasAcquiredTarget && !currentData.isSet(TV)) {
+            // If we've already seen the target and lose tracking, exit.
+        } else if (mHasAcquiredTarget && !currentData.isSet(TV)) {
             return true;
         }
 
-        mPreviousTime = pNow;
-        
-         //command has not completed
-        return false;                                                      
+        //command has not completed
+        return false;
     }
 
     @Override
@@ -87,18 +63,7 @@ public class TargetLock implements ICommand {
         Robot.DATA.drivetrain.set(EDriveData.DESIRED_STATE, EDriveState.NORMAL);
     }
 
-    public TargetLock setTargetLockThrottleProvider(IThrottleProvider pThrottleProvider) {
-//        this.mTargetLockThrottleProvider = pThrottleProvider;
-        return this;
+    public void setEndOnAlignment(boolean bool) {
+        mEndOnAlignment = bool;
     }
-
-    public TargetLock setTargetSearchThrottleProvider(IThrottleProvider pThrottleProvider) {
-//        this.mTargetSearchThrottleProvider = pThrottleProvider;
-        return this;
-    }
-    public TargetLock setStopWhenTargetLost(boolean pStopWhenTargetLost) {
-        this.mStopWhenTargetLost = pStopWhenTargetLost;
-        return this;
-    }
-
 }
