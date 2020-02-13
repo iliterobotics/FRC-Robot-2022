7106bcda4651d6209332ce6835304c47923c8776
Agrim Sharma <76flyershd@gmail.com>
2020-02-13 18:56:08 -0500

    network-table entry for slider

--- robot/src/main/java/us/ilite/robot/modules/OperatorInput.java
+++ robot/src/main/java/us/ilite/robot/modules/OperatorInput.java
@@ -4,12 +4,15 @@ import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import us.ilite.common.types.EMatchMode;
 import us.ilite.common.types.input.ELogitech310;
 import us.ilite.robot.Robot;
 
 import java.util.List;
+import java.util.Map;
 
 public class OperatorInput extends Module {
     protected static final double
@@ -20,13 +23,20 @@ public class OperatorInput extends Module {
     private Joystick mOperatorJoystick;
     private EMatchMode mMode = EMatchMode.DISABLED;
 
-    private ShuffleboardTab AutonPathDropdown;
+    private ShuffleboardTab mAutonConfiguration;
+    private NetworkTableEntry AutonPathDropdown;
     private NetworkTableEntry mMatchTimeEntry;
 
+
     public OperatorInput() {
-        mMatchTimeEntry = AutonPathDropdown.add("Match Time", 0).withSize(2, 1).
+        mMatchTimeEntry = mAutonConfiguration.add("Match Time", 0).withSize(2, 1).
                 withPosition(0, 0).getEntry();
         mDriverJoystick = new Joystick(0);
+        AutonPathDropdown = Shuffleboard.getTab("Auton Options").add("Option", 0)
+                .withWidget(BuiltInWidgets.kNumberSlider)
+                .withProperties(Map.of("min", 0, "max", 5))
+                .getEntry();
+
         mOperatorJoystick = new Joystick(1);
     }
 
@@ -50,6 +60,7 @@ public class OperatorInput extends Module {
 
     @Override
     public void setOutputs(double pNow) {
+
     }
 
 }
