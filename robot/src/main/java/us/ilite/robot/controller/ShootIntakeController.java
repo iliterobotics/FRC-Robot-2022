dcbfbba85bd62c65fbe22039f525e21f2671415c
Agrim Sharma <76flyershd@gmail.com>
2020-02-17 12:37:11 -0500

    WIP

--- robot/src/main/java/us/ilite/robot/controller/ShootIntakeController.java
+++ robot/src/main/java/us/ilite/robot/controller/ShootIntakeController.java
@@ -1,26 +1,29 @@
 package us.ilite.robot.controller;
 
-import com.team2363.commands.HelixFollower;
-import com.team2363.commands.IliteHelixFollower;
-import com.team2363.controller.PIDController;
-import com.team2363.logger.HelixLogger;
 import com.team319.trajectory.Path;
-import us.ilite.common.types.drive.EDriveData;
-import us.ilite.common.types.sensor.EGyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import us.ilite.robot.Robot;
 import us.ilite.robot.auto.paths.BobUtils;
 import us.ilite.robot.auto.paths.SimpleSequence;
-import us.ilite.robot.modules.EDriveState;
+import us.ilite.robot.auto.paths.T_LINE_10_FT;
 
 public class ShootIntakeController extends BaseAutonController {
-    protected Path mActivePath = null;
-    protected double mPathStartTime = 0d;
     private double mPathDistance = 0d;
     private SimpleSequence mSimpleSequence = new SimpleSequence();
+
+    public ShootIntakeController() {
+        setActivePath(new T_LINE_10_FT());
+    }
     @Override
     protected void updateImpl(double pNow) {
         super.updateImpl(pNow);
-        mSimpleSequence.updateSequence(pNow, mPathDistance);
+        int pathIndex = BobUtils.getIndexForCumulativeTime(mActivePath, pNow, mPathStartTime);
+        if (pathIndex != -1 ){
+            mPathDistance = mActivePath.getPath()[pathIndex][7];
+        } else {
+            SmartDashboard.putNumber("PATH DISTANCE", mPathDistance);
+            mSimpleSequence.updateSequence(pNow, mPathDistance);
+        }
     }
 
 }
