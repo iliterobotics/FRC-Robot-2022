7567981bc2308a9e10ff62708304939e82da7b5c
John Anselmo <jjohnanselmo@gmail.com>
2020-02-22 12:02:29 -0500

    Implementation of abstractcontroller logic for sequencing

--- robot/src/main/java/us/ilite/robot/controller/DriveStraightController.java
+++ robot/src/main/java/us/ilite/robot/controller/DriveStraightController.java
@@ -4,10 +4,10 @@ import us.ilite.common.Data;
 import us.ilite.common.Distance;
 import us.ilite.common.types.EPowerCellData;
 import us.ilite.common.types.drive.EDriveData;
+import us.ilite.robot.Enums;
 import us.ilite.robot.Robot;
 
-import static us.ilite.common.types.drive.EDriveData.L_ACTUAL_POS_FT;
-import static us.ilite.common.types.drive.EDriveData.R_ACTUAL_POS_FT;
+import static us.ilite.common.types.drive.EDriveData.*;
 
 public class DriveStraightController extends BaseAutonController {
     private Distance targetDistance;
@@ -15,9 +15,11 @@ public class DriveStraightController extends BaseAutonController {
     private boolean isFirstLegDone;
     
     public DriveStraightController(Distance mDistance){
+        db.drivetrain.set(STATE, Enums.EDriveState.PERCENT_OUTPUT);
         this.targetDistance = mDistance;
         isFirstLegDone = false;
     }
+
     @Override
     protected void updateImpl(double pNow) {
         super.updateImpl(pNow);
@@ -26,25 +28,24 @@ public class DriveStraightController extends BaseAutonController {
             db.drivetrain.set(EDriveData.DESIRED_THROTTLE_PCT, 0.5);
             isFirstLegDone = true;
         }
+        activateSerializer(pNow);
+        setIntakeArmEnabled(pNow, true);
         initiateAllModules(isFirstLegDone);
     }
+
     public Distance getDistance() {
         return Distance.fromFeet(
                 (Robot.DATA.drivetrain.get(L_ACTUAL_POS_FT) +
                         Robot.DATA.drivetrain.get(R_ACTUAL_POS_FT)) / 2.0);
     }
+
     public boolean isAtDistance(Distance targetDistance){
-        if (getDistance().inches() >= targetDistance.inches()){
-            return true;
-        }
-        return false;
+        return getDistance().inches() >= targetDistance.inches();
     }
+
     public void initiateAllModules(boolean isFirstLegDone){
         if(isFirstLegDone){
-            db.powercell.set(EPowerCellData.INTAKE_STATE , 0.5);
             //TODO implement flywheel module stuff
-            db.powercell.set(EPowerCellData.DESIRED_H_VELOCITY , 0.5);
-            db.powercell.set(EPowerCellData.DESIRED_V_VELOCITY , 0.5);
 
         }
     }
