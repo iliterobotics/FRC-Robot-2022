0647c00fe9880046289aa1ec8dcd37af9ee2a72b
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2020-02-21 20:03:34 -0500

    Tested New Routines w/ Working Reversing

--- robot/src/main/java/us/ilite/robot/controller/YoinkController.java
+++ robot/src/main/java/us/ilite/robot/controller/YoinkController.java
@@ -1,32 +1,37 @@
 package us.ilite.robot.controller;
 
-import com.team2363.commands.IliteHelixFollower;
 import com.team319.trajectory.Path;
-import us.ilite.robot.auto.paths.BobUtils;
-import us.ilite.robot.auto.paths.YoinkFrom;
-import us.ilite.robot.auto.paths.YoinkTo;
+import us.ilite.robot.auto.paths.*;
 
 public class YoinkController extends BaseAutonController {
-
-    private Path mCurrentPath = new YoinkTo();
-
     private double mStartTime;
 
-    private boolean isAtTrench(double pNow) {
-        return BobUtils.getIndexForCumulativeTime(new YoinkTo(), pNow, mStartTime) == new YoinkTo().getPath().length - 1;
+    private boolean isFinished(double pNow, Path pPath) {
+        return BobUtils.getIndexForCumulativeTime(pPath, pNow, mStartTime) == pPath.getPath().length - 1;
     }
 
     public YoinkController(double pNow) {
         mStartTime = pNow;
-        setActivePath(mCurrentPath);
+        setActivePath(new GoToTrenchFromLine());
+        System.out.println("AUTONOMOUS ROUTINE HAS BEEN STARTED");
     }
 
     @Override
     public void updateImpl(double pNow) {
-        if (isAtTrench(pNow)) {
-            setActivePath(new YoinkFrom());
+        if (isFinished(pNow, new GoToTrenchFromLine())) {
+            setActivePath(new ReturnToShooting());
             mPathFollower.reverse();
+            System.out.println("ROBOT IS AT TRENCH");
         }
+        if (isFinished(pNow, new ReturnToShooting())) {
+            setActivePath(new PickUpBallsFromGenerator());
+            System.out.println("ROBOT IS BACK TO THE INITIATION LINE");
+        }
+        if (isFinished(pNow, new PickUpBallsFromGenerator())) {
+            setActivePath(new FinalShots());
+            System.out.println("ROBOT IS AT GENERATOR");
+        }
+        System.out.println("AUTONOMOUS ROUTINE HAS BEEN FINISHED");
         super.updateImpl(pNow);
     }
 }
