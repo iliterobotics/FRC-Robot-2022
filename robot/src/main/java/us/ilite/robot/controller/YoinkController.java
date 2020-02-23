c58b62074d7f8b7d91f34e2ab150149ff90bb8e0
Agrim Sharma <76flyershd@gmail.com>
2020-02-23 10:30:19 -0500

    new methods

--- robot/src/main/java/us/ilite/robot/controller/YoinkController.java
+++ robot/src/main/java/us/ilite/robot/controller/YoinkController.java
@@ -4,6 +4,7 @@ import com.team2363.commands.IliteHelixFollower;
 import com.team319.trajectory.Path;
 import us.ilite.common.types.EPowerCellData;
 import us.ilite.common.types.EShooterSystemData;
+import us.ilite.robot.Enums;
 import us.ilite.robot.auto.paths.*;
 
 public class YoinkController extends BaseAutonController {
@@ -18,7 +19,6 @@ public class YoinkController extends BaseAutonController {
 
     private boolean isFinished(double pNow, Path pPath) {
         double dt = pNow - mPathStartTime;
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + dt + "\n" +( BobUtils.getPathTotalTime(pPath) )+ "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ");
         return BobUtils.getIndexForCumulativeTime(pPath, pNow, mPathStartTime) == -1;
     }
 
@@ -32,20 +32,19 @@ public class YoinkController extends BaseAutonController {
             // Update again since path has changed, follows process of BaseAutonController
             super.updateImpl(pNow);
         }
-        activateSerializer(pNow);
-        setIntakeArmEnabled(pNow , true);
+        startInhaling(pNow);
+        startShooting(pNow);
     }
-//    private void startInhaling(double pNow){
-//        if(isFinished(pNow, mYoinkTO)){
-//            db.powercell.set(EPowerCellData.ARM_ANGLE_deg , 0);
-//            db.powercell.set(EPowerCellData.SET_H_pct , 0.5);
-//            db.powercell.set(EPowerCellData.SET_V_pct , 0.5);
-//        }
-//    }
-//    private void startShooting(double pNow){
-//        if(isFinished(pNow , mYoinkFrom)) {
-//            db.flywheel.set(EShooterSystemData.SET_FEEDER_rpm, 6000);
-//            db.flywheel.set(EShooterSystemData.SET_BALL_VELOCITY_ft_s, 6000);
-//        }
-//    }
+    private void startInhaling(double pNow){
+        if(isFinished(pNow, mYoinkTO)){
+            setIntakeArmEnabled(pNow , true);
+            activateSerializer(pNow);
+        }
+    }
+    private void startShooting(double pNow){
+        if(isFinished(pNow , mYoinkFrom)) {
+           setFlywheelClosedLoop(Enums.FlywheelSpeeds.FAR);
+           setFeederClosedLoop(Enums.FlywheelSpeeds.FAR);
+        }
+    }
 }
