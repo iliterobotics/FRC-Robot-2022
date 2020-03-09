a1226bdf41692f4fe7dfaec01dc08ca6615f2d02
Jesse Knight <flybotix@gmail.com>
2020-03-09 14:40:41 -0400

    Fixed compile error

--- common/src/main/java/us/ilite/common/types/EHangerModuleData.java
+++ common/src/main/java/us/ilite/common/types/EHangerModuleData.java
@@ -1,7 +1,7 @@
 package us.ilite.common.types;
 
 public enum EHangerModuleData {
-    L_VEL_rpm,,R_VEL_rpm,
+    L_VEL_rpm,R_VEL_rpm,
     L_POSITION_rot,R_POSITION_rot,
     L_OUTPUT_CURRENT,R_OUTPUT_CURRENT,
     SET_pct,
