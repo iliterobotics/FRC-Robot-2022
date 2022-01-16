07ea6ec4b8dfe8497a90144af624bfbea21dc103
Agrim Sharma <75flyershd@gmail.com>
2022-01-16 14:32:46 -0500

    fixed enums

--- src/main/java/us/ilite/robot/Enums.java
+++ src/main/java/us/ilite/robot/Enums.java
@@ -62,111 +62,29 @@ public class Enums {
         SMART_MOTION
     }
 
-    // =============================================================================
-    // DJ Spinner States
-    // =============================================================================
-    public enum EColorMatch {
-        NONE(0, 0, 0 ),
-        RED( 0.561, 0.232, 0.114 ),
-        YELLOW( 0.361, 0.524, 0.113 ),
-        BLUE(  0.143, 0.427, 0.429 ),
-        GREEN(  0.197, 0.561, 0.240 );
-        public Color color;
-        public double r;
-        public double g;
-        public double b;
-
-        EColorMatch(double r, double g, double b) {
-            color = new Color(r,g,b);
-        }
-
-//        public Color getColorMatch() {
-//            return ColorMatch.makeColor(r, g, b);
-//        }
-
-        public EColorMatch nextColor() {
-            if(this.ordinal() == GREEN.ordinal()) {
-                return RED;
-            } else if (this.ordinal() == NONE.ordinal() ){
-                return NONE;
-            }
-            else {
-                return EColorMatch.values()[ordinal()+1];
-            }
-        }
-
-        public static EColorMatch from(ColorMatchResult pResult) {
-            for(EColorMatch cm : values()) {
-                if(equal(cm.color, pResult.color)) {
-                    return cm;
-                }
-            }
-            return NONE;
-        }
-
-        private static boolean equal(Color a, Color b) {
-            //TODO - this is where we put the thresholding for different color
-            // e.g. if abs(a.red - b.red) <= 0.1 then they are close
-            return a.equals(b);
-        }
-    }
-
-    public enum EColorWheelState {
-        OFF (0.0),
-        ROTATION (0.4),
-        POSITION (0.2);
-
-        public double power;
-        private EColorWheelState(double _power) {
-            power = _power;
-        }
-
-        public double getPower(){
-            return this.power;
-        }
-        public static EColorWheelState valueOf(double pOrdinal) {
-            return values()[(int)pOrdinal];
-        }
-    }
-
-
 
     // =============================================================================
     // Flywheel States
     // =============================================================================
-    public enum HoodState {
-        NONE,
-        TARGET_ANGLE,
-        MANUAL
-    }
-    public enum EHoodSensorError {
-        NONE,
-        INVALID_POTENTIOMETER_READING
-    }
     public enum FlywheelWheelState {
         NONE,
         OPEN_LOOP,
         VELOCITY
-    }
-    public enum TurretControlType {
-        MANUAL,
-        TARGET_LOCKING,
-        HOME
     }
 
     /**
      * Closed-loop velocity settings for various target speeds
      */
     public enum FlywheelSpeeds {
-        OFF(                00.0, 62.0, 0.00, HoodState.NONE, FlywheelWheelState.NONE),
-        HOME(               0.0, 30.0, 0.00, HoodState.TARGET_ANGLE, FlywheelWheelState.NONE),
-        DEFAULT(            20.0, 45.0, 0.75, HoodState.TARGET_ANGLE, FlywheelWheelState.VELOCITY),
-        CLOSE(              24.0, 62.0, 0.75, HoodState.TARGET_ANGLE, FlywheelWheelState.VELOCITY),
-        INITIATION_LINE(    29.0, 39.5, 0.75, HoodState.TARGET_ANGLE, FlywheelWheelState.VELOCITY),
-        FAR(                35.0, 31.5 * 1.1, 0.75, HoodState.TARGET_ANGLE, FlywheelWheelState.VELOCITY),
-        FAR_TRENCH(         52.0, 22.0 * 1.1, 0.75, HoodState.TARGET_ANGLE, FlywheelWheelState.VELOCITY),
+        OFF(                00.0, 62.0, 0.00, FlywheelWheelState.NONE),
+        HOME(               0.0, 30.0, 0.00, FlywheelWheelState.NONE),
+        DEFAULT(            20.0, 45.0, 0.75, FlywheelWheelState.VELOCITY),
+        CLOSE(              24.0, 62.0, 0.75, FlywheelWheelState.VELOCITY),
+        INITIATION_LINE(    29.0, 39.5, 0.75, FlywheelWheelState.VELOCITY),
+        FAR(                35.0, 31.5 * 1.1, 0.75, FlywheelWheelState.VELOCITY),
+        FAR_TRENCH(         52.0, 22.0 * 1.1, 0.75, FlywheelWheelState.VELOCITY),
         /** Override represents a highly-variable angle / speed state where the controller must directly set everything */
-        OVERRIDE(           00.0, 45.0, 0.00, HoodState.TARGET_ANGLE, FlywheelWheelState.VELOCITY);
+        OVERRIDE(           00.0, 45.0, 0.00, FlywheelWheelState.VELOCITY);
 
         /**
          * Ball trajectory exit speed in feet per second
@@ -182,13 +100,11 @@ public class Enums {
          * Feeder wheel PCT_OUTPUT, {-1,1}
          */
         public double feeder;
-        public HoodState hoodstate;
         public FlywheelWheelState wheelstate;
-        FlywheelSpeeds(double _speed, double _angle, double _feeder, HoodState _hoodstate, FlywheelWheelState _wheelstate){
+        FlywheelSpeeds(double _speed, double _angle, double _feeder, FlywheelWheelState _wheelstate){
             speed=_speed;
             angle=_angle;
             feeder=_feeder;
-            hoodstate = _hoodstate;
             wheelstate = _wheelstate;
         }
     }
@@ -214,21 +130,7 @@ public class Enums {
             return pPower;
         }
     }
-
-    public enum EArmState {
-        NONE(0.0, 0),
-        OUT(90.0, 1),
-        // TODO - fix to UP slot
-        STOW(0.0, 1),
-        HOLD(0.0, 1);
-
-        public double angle;
-        public int slot;
-
-        EArmState (double angle, int slot) {
-            this.angle = angle;
-        }
-    }
+    
 
     public enum EIndexingState {
         BROKEN (true),
