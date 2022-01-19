8da376ef57b1b33f579eb9c2aa2dac427ba5b0b7
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2022-01-18 22:22:50 -0500

    Hold mode, Velocity output, Percent Output, and Position Output ready for testing

--- src/main/java/us/ilite/robot/modules/DriveModule.java
+++ src/main/java/us/ilite/robot/modules/DriveModule.java
@@ -9,6 +9,7 @@ import us.ilite.common.lib.control.PIDController;
 import us.ilite.common.lib.control.ProfileGains;
 import us.ilite.common.types.EMatchMode;
 import us.ilite.common.types.EVisionGoal2020;
+import us.ilite.common.types.drive.EDriveData;
 import us.ilite.common.types.sensor.EPowerDistPanel;
 import us.ilite.robot.Enums.EDriveState;
 import us.ilite.robot.Robot;
@@ -60,15 +61,17 @@ public class DriveModule extends Module {
 
 	public static PIDController vPID, dPID, mTargetAngleLockPid;
 
-	private boolean mStartHoldingPosition = false;
-	private double mLeftHoldSetpoint;
-	private double mRightHoldSetpoint;
+	private static double mLeftHoldPosition = 0;
+	private static double mRightHoldPosition = 0;
+	private static int mCyclesHolding = 0;
 
 	public static ProfileGains kTurnToProfileGains = new ProfileGains().f(0.085);
 	public static double kTurnSensitivity = 0.85;
 
 	public static ProfileGains kDriveHeadingGains = new ProfileGains().p(0.03);
 	public static ProfileGains kYawGains = new ProfileGains().p(0.1);
+
+
 
 	private final ProfileGains vGains = new ProfileGains()
 			.p(0.0001)
@@ -108,7 +111,6 @@ public class DriveModule extends Module {
 		mTargetAngleLockPid.setOutputRange(Settings.kTargetAngleLockMinPower, Settings.kTargetAngleLockMaxPower);
 		mTargetAngleLockPid.setSetpoint(0);
 		mTargetAngleLockPid.reset();
-		mStartHoldingPosition = false;
 	}
 
 	@Override
@@ -155,9 +157,9 @@ public class DriveModule extends Module {
 				mRightMaster.set(ControlMode.PercentOutput, right);
 				break;
 			case VELOCITY:
-				mLeftMaster.set(ControlMode.Velocity, vPID.calculate(left, clock.dt()));
-				mRightMaster.set(ControlMode.Velocity, vPID.calculate(right, clock.dt()));
-				mStartHoldingPosition = false;
+				mLeftMaster.set(ControlMode.Velocity, vPID.calculate(left*kDriveTrainMaxVelocityRPM, clock.dt()));
+				mRightMaster.set(ControlMode.Velocity, vPID.calculate(right*kDriveTrainMaxVelocityRPM, clock.dt()));
+				mCyclesHolding = 0;
 				break;
 			case TARGET_ANGLE_LOCK:
 				double pidOutput = 0;
@@ -168,35 +170,42 @@ public class DriveModule extends Module {
 //					SmartDashboard.putNumber("Target Angle Lock PID Output", pidOutput);
 					turn = pidOutput;
 				}
-				mLeftMaster.set(ControlMode.Position, pidOutput);
-				mRightMaster.set(ControlMode.Position, pidOutput);
+				mLeftMaster.set(ControlMode.Velocity, pidOutput);
+				mRightMaster.set(ControlMode.Velocity, -pidOutput);
 				break;
 			case HOLD:
-				if (!mStartHoldingPosition) {
-					mLeftHoldSetpoint = db.drivetrain.get(L_ACTUAL_POS_FT);
-					mRightHoldSetpoint = db.drivetrain.get(R_ACTUAL_POS_FT);
-					mStartHoldingPosition = true;
+				if (mCyclesHolding == 0) {
+					mLeftHoldPosition = db.drivetrain.get(L_ACTUAL_POS_FT);
+					mRightHoldPosition = db.drivetrain.get(R_ACTUAL_POS_FT);
 				}
 
+				double mCurrentLeftPosition = db.drivetrain.get(L_ACTUAL_POS_FT);
+				double mCurrentRightPosition = db.drivetrain.get(R_ACTUAL_POS_FT);
+
+				double deltaLeft = mCurrentLeftPosition - mLeftHoldPosition;
+				double deltaRight = mCurrentRightPosition - mRightHoldPosition;
+
 				if (db.drivetrain.get(L_ACTUAL_VEL_FT_s) < 100) {
-					if (Math.abs(db.drivetrain.get(L_ACTUAL_POS_FT) - mLeftHoldSetpoint) > .15) {
-						mLeftMaster.set(ControlMode.Position, dPID.calculate(mLeftHoldSetpoint, clock.dt()));
+					if (Math.abs(deltaLeft) >= 0.1) {
+						mLeftMaster.set(ControlMode.Position, dPID.calculate(mLeftHoldPosition, clock.dt()));
+					} else {
+						mLeftMaster.set(ControlMode.Velocity, 0);
 					}
-				} else {
-					mLeftMaster.set(ControlMode.Velocity, 0);
 				}
 
 				if (db.drivetrain.get(R_ACTUAL_VEL_FT_s) < 100) {
-					if (Math.abs(db.drivetrain.get(R_ACTUAL_POS_FT) - mRightHoldSetpoint) > .15) {
-						mRightMaster.set(ControlMode.Position, dPID.calculate(mRightHoldSetpoint, clock.dt()));
+					if (Math.abs(deltaRight) >= 0.1) {
+						mRightMaster.set(ControlMode.Position, dPID.calculate(mRightHoldPosition, clock.dt()));
+					} else {
+						mRightMaster.set(ControlMode.Velocity, 0);
 					}
-				} else {
-					mRightMaster.set(ControlMode.Velocity, 0);
 				}
+
+				mCyclesHolding++;
 				break;
 			case SMART_MOTION:
-				mLeftMaster.set(ControlMode.Position, dPID.calculate(left, clock.dt()));
-				mRightMaster.set(ControlMode.Position, dPID.calculate(right, clock.dt()));
+				mLeftMaster.set(ControlMode.Position, dPID.calculate(db.drivetrain.get(L_DESIRED_POS), clock.dt()));
+				mRightMaster.set(ControlMode.Position, dPID.calculate(db.drivetrain.get(R_DESIRED_POS), clock.dt()));
 				break;
 			default:
 				mLeftMaster.set(ControlMode.PercentOutput, 0.0);
