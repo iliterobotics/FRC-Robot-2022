1a50c87c686a224e7aaa921ea0e9c92d933ba340
Megan Farran <farranme23@pwcs-edu.org>
2022-01-20 20:21:24 -0800

    Added some PID

--- src/main/java/us/ilite/robot/modules/ClimberModule.java
+++ src/main/java/us/ilite/robot/modules/ClimberModule.java
@@ -1,6 +1,8 @@
 package us.ilite.robot.modules;
 
 import com.revrobotics.*;
+import edu.wpi.first.math.controller.PIDController;
+import us.ilite.common.lib.control.ProfileGains;
 import us.ilite.common.types.EHangerModuleData;
 import us.ilite.robot.hardware.SparkMaxFactory;
 
@@ -8,10 +10,16 @@ public class ClimberModule extends Module{
 
     private CANSparkMax mSparkMaxOne;
     private RelativeEncoder mEncdoerSparkMaxOne;
+    private PIDController mHangerPid;
+    private ProfileGains kHangerProfile;
+    private double maxVelocity;
+    private double minVelocity;
 
     public ClimberModule() {
         mSparkMaxOne = SparkMaxFactory.createDefaultSparkMax(9, CANSparkMaxLowLevel.MotorType.kBrushless);
         mEncdoerSparkMaxOne = mSparkMaxOne.getEncoder();
+        kHangerProfile = new ProfileGains().p(.001).i(0).d(0);
+        mHangerPid = new PIDController(kHangerProfile, -maxVelocity, minVelocity, clock.dt());
     }
     @Override
     public void readInputs() {
@@ -21,5 +29,6 @@ public class ClimberModule extends Module{
     @Override
     public void setOutputs() {
         mSparkMaxOne.set(db.hanger.get(EHangerModuleData.SET_pct));
+        double desiredVelocity = mHangerPid.
     }
 }
