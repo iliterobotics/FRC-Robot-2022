c10ed76b2f21c2b30da28f516a00005ec2e29a08
Christopher O'Connell <coconnell84@gmail.com>
2022-01-29 17:19:55 -0500

    Added documentation

--- src/main/java/us/ilite/robot/controller/BaseAutonController.java
+++ src/main/java/us/ilite/robot/controller/BaseAutonController.java
@@ -20,21 +20,65 @@ import us.ilite.common.types.drive.EDriveData;
 import us.ilite.robot.Enums;
 import us.ilite.robot.TrajectoryCommandUtils;
 
+/**
+ * Class responsible for executing {@link Trajectory} and moving the
+ * robot along said Trajectory. This is an autonmous routine that can
+ * accepty and Trajectory, so long as it is feasible (mathematically and
+ * realistically).
+ */
 public class BaseAutonController extends AbstractController {
 
+    /**
+     * Timer used to get clock time in seconds. This is the time off of the
+     * robot and usually doesn't represent real time. This is only intended to be
+     * used for temporal offsets.
+     */
     private final Timer mTimer;
+    /**
+     * Flag indiciating if this run should use PID in it's calculation
+     */
     private final boolean mUsePid;
+    /**
+     * The {@link RamseteController} that is used to follow a {@link Trajectory}. This
+     * will be used to calculate the chasis speed based on the robots pose and velocity
+     * at a given time. It will also look up where the robot should be along the trajectory
+     */
     private final RamseteController mFollower;
+    /**
+     * The module responsible for calculating how much volatage to send to the motors based on the
+     * desired velocity.
+     */
     private final SimpleMotorFeedforward mFeedforward;
+    /**
+     * The kinematics of the motors based on the distance between the wheels (left and right).
+     */
     private final DifferentialDriveKinematics mDriveKinematics;
+    /**
+     * Left Motor PID Controller
+     */
     private final PIDController mLeftController;
+    /**
+     * Right Motor PID Controller
+     */
     private final PIDController mRightController;
-
-    //TODO figure out way to store trajectories and switch between them
+    /**
+     * The trajectory to execute. At this time this class reaches out and gets the trajectory. Since
+     * the trajectory needs to be restarted every time, this is not final and is reloaded in the init method.
+     */
     private Trajectory mTrajectory;
+    /**
+     * A history of the speeds of the wheels this module has calculated for the trajectory
+     */
     private DifferentialDriveWheelSpeeds mPrevSpeeds;
+    /**
+     * The time, in seconds. This may not reflect realtime.
+     */
     private double mPrevTime;
 
+    /**
+     * Default constructor. This will instantiate the variables that are not dependent on the init
+     * state of autonomous. For those components, those will be initialized in the initialized method
+     */
     public BaseAutonController() {
         mUsePid = true;
         mFollower = new RamseteController(Settings.kRamseteB, Settings.kRamseteZeta);
@@ -47,6 +91,12 @@ public class BaseAutonController extends AbstractController {
 
 
     }
+
+    /**
+     * Method that should be called when the autonmous state is initialized. This is so that the overall
+     * state can be restarted, should the autonomous be run multiple times. (Something that only really happens
+     * at home).
+     */
     public void initialize() {
         mTimer.reset();
         mTimer.start();
@@ -74,7 +124,12 @@ public class BaseAutonController extends AbstractController {
         execute();
     }
 
-    public void execute() {
+    /**
+     * Method to perform the actual traversal. This should run a single step. A step will be defined as
+     * a sequence of execution between some delta time from the previous execution. This method is expected to be
+     * called multiple times until the robot traverses the entire Trajectory or until autonmous runs out of time.
+     */
+    private void execute() {
         db.drivetrain.set(EDriveData.STATE, Enums.EDriveState.PATH_FOLLOWING_RAMSETE);
         double curTime = mTimer.get();
         double dt = curTime - mPrevTime;
@@ -108,41 +163,103 @@ public class BaseAutonController extends AbstractController {
         mPrevTime = curTime;
     }
 
-    private double calculateFeedsForward(double speedSetpoint, double prevSpeedsMetersPerSec, double dt) {
+    /**
+     * Method to calculate the feed forward step of the trajectory traversal
+     * @param pSpeedSetpoint The speed set point
+     * @param pPrevSpeedsMetersPerSec The previous speed in meters/sec
+     * @param pDt The amount of time that has passed since the last execution
+     * @return The feeds forward from the gains and setpoint
+     */
+    private double calculateFeedsForward(double pSpeedSetpoint, double pPrevSpeedsMetersPerSec, double pDt) {
         return mFeedforward.calculate(
-                speedSetpoint, (speedSetpoint - prevSpeedsMetersPerSec) / dt);
-    }
-    private double calculateOutputFromFeedForward(double feedForward, PIDController pidController, double actualSpeeds, double setPoint) {
-        return feedForward + pidController.calculate(actualSpeeds, setPoint);
+                pSpeedSetpoint, (pSpeedSetpoint - pPrevSpeedsMetersPerSec) / pDt);
     }
 
-    private DifferentialDriveWheelSpeeds calculateActualSpeeds(double leftSpeedSetpoint, double rightSpeedSetpoint) {
+    /**
+     * Method to calculate the feed foward plus the pid output from actual speed and setpoint
+     * @param pFeedForward
+     *  The feed forwards
+     * @param pPidController
+     *  the PID controller
+     * @param pActualSpeeds
+     *  The actual speeds (m/s)
+     * @param pSetPoint
+     *  The set point
+     * @return
+     *  The feed forward + the output from the pid controller given the actual speed and setpoints
+     */
+    private double calculateOutputFromFeedForward(double pFeedForward, PIDController pPidController, double pActualSpeeds, double pSetPoint) {
+        return pFeedForward + pPidController.calculate(pActualSpeeds, pSetPoint);
+    }
+
+    /**
+     * Method to calcualte the actual speeds from the set points
+     * @param pLeftSpeedSetpoint
+     *  The left setpoint
+     * @param pRightSpeedSetpoint
+     *  The right setpoint
+     * @return
+     *  Return the {@link DifferentialDriveWheelSpeeds}
+     */
+    private DifferentialDriveWheelSpeeds calculateActualSpeeds(double pLeftSpeedSetpoint, double pRightSpeedSetpoint) {
 
         double actualLeftSpeed = Units.feet_to_meters(db.drivetrain.get(EDriveData.L_ACTUAL_VEL_FT_s));
         double actualRightSpeed = Units.feet_to_meters(db.drivetrain.get(EDriveData.R_ACTUAL_VEL_FT_s));
         return new DifferentialDriveWheelSpeeds(actualLeftSpeed, actualRightSpeed);
     }
 
-    private DifferentialDriveWheelSpeeds getTargetWheelSpeeds(double curTime, Pose2d robotPose) {
+    /**
+     * Method to get the target wheel speeds
+     * @param pCurTime
+     *  The current time in seconds
+     * @param pRobotPose
+     *  The current robot pose
+     * @return
+     *  The robot wheel speeds
+     */
+    private DifferentialDriveWheelSpeeds getTargetWheelSpeeds(double pCurTime, Pose2d pRobotPose) {
         return mDriveKinematics.toWheelSpeeds(
-                mFollower.calculate(robotPose, mTrajectory.sample(curTime)));
+                mFollower.calculate(pRobotPose, mTrajectory.sample(pCurTime)));
     }
 
+
+    /**
+     * Get the current robot opose
+     * @return
+     *  The robot pose represented as {@link Pose2d}
+     */
     private Pose2d getRobotPose() {
         Rotation2d r2d = new Rotation2d(Units.degrees_to_radians(db.drivetrain.get(EDriveData.DELTA_HEADING)));
         Pose2d robotPose = new Pose2d(db.drivetrain.get(EDriveData.GET_X_OFFSET), db.drivetrain.get(EDriveData.GET_Y_OFFSET), r2d);
         return robotPose;
     }
 
+    /**
+     * Method to update the motors
+     * @param pOutput
+     *  The voltage values to set the motors
+     */
     private void updateDriveTrain(Pair<Double,Double> pOutput) {
         System.out.println("BaseAutonController: Setting desired voltage= ["+pOutput+"]");
-        db.drivetrain.set(EDriveData.DESIRED_LEFT_VOLTAGE, pOutput.getFirst());
-        db.drivetrain.set(EDriveData.DESIRED_RIGHT_VOLTAGE, pOutput.getSecond());
+        db.drivetrain.set(EDriveData.DESIRED_LEFT_VOLTAGE, pOutput.getLeft());
+        db.drivetrain.set(EDriveData.DESIRED_RIGHT_VOLTAGE, pOutput.getRight());
 
     }
+
+    /**
+     * Method to check if the trajectory is finished
+     * @return
+     *  true if the trajectory traversal is complete, false otherwise
+     */
     public boolean isFinished() {
         return mTimer.hasElapsed(mTrajectory.getTotalTimeSeconds());
     }
+
+    /**
+     * Method to stop
+     * @param interrupted
+     *  If true, this will force stop
+     */
     public void end(boolean interrupted) {
         mTimer.stop();
         if (interrupted) {
