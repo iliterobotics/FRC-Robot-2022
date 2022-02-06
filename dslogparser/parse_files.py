215b477a7c1196db4eb39d065e75fb37e88b8d3b
Christopher O'Connell <coconnell84@gmail.com>
2022-02-06 11:08:50 -0500

    Copied over source from the dsloparse and wrote a conversion utility to CSV

---
+++ dslogparser/parse_files.py
@@ -0,0 +1,41 @@
+import csv
+import sys, getopt
+from dslogparser import DSEventParser
+
+def main(argv):
+    inputfile = None
+    outputfile = None
+
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+        print(opts)
+
+        for opt, arg in opts:
+            print(opt,arg)
+
+            if opt == '-h':
+                print('parse_files.py -i <inputfile> -o <outputfile>')
+            elif opt in ('-i','--ifile'):
+                inputfile = arg
+            elif opt in ('-o','--ofile'):
+                outputfile = arg
+        
+        if inputfile and outputfile:
+            parser = DSEventParser(inputfile)
+            records = parser.read_records()
+
+            with open(outputfile,'w',newline='') as csv_out:
+                csv_writer = csv.writer(csv_out)
+    
+                for r in records:
+                    csv_writer.writerow([r['time'],r['message']])
+        else:
+            print('need an input and an output')
+    
+    except getopt.GetoptError:
+        print('parse_files -i <input_file> -o <outputfile>')
+        
+        
+
+if __name__ =='__main__':
+    main(sys.argv[1:])
