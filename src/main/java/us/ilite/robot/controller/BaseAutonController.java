62b80a200267c4f7157264258601c80b374e417f
Christopher O'Connell <coconnell84@gmail.com>
2022-02-11 17:33:43 -0500

    Revert the robot pose

--- src/main/java/us/ilite/robot/controller/BaseAutonController.java
+++ src/main/java/us/ilite/robot/controller/BaseAutonController.java
@@ -306,8 +306,8 @@ public class BaseAutonController extends AbstractController {
      */
     private Pose2d getRobotPose() {
         Rotation2d r2d = new Rotation2d(db.drivetrain.get(EDriveData.ACTUAL_HEADING_RADIANS));
-        Pose2d robotPose = new Pose2d();
-//                new Pose2d(db.drivetrain.get(EDriveData.GET_X_OFFSET_METERS), db.drivetrain.get(EDriveData.GET_Y_OFFSET_METERS), r2d);
+        Pose2d robotPose =
+                new Pose2d(db.drivetrain.get(EDriveData.GET_X_OFFSET_METERS), db.drivetrain.get(EDriveData.GET_Y_OFFSET_METERS), r2d);
 
         return robotPose;
     }
