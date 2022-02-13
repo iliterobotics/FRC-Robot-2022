5f44e87f9b7ceee1df6d4c98e6eaa1111f4fd020
Christopher O'Connell <coconnell84@gmail.com>
2022-02-13 14:26:35 -0500

    Change PID to (3,0,0.005)

--- src/main/java/us/ilite/robot/controller/BaseAutonController.java
+++ src/main/java/us/ilite/robot/controller/BaseAutonController.java
@@ -96,8 +96,8 @@ public class BaseAutonController extends AbstractController {
     public BaseAutonController() {
         mFollower = new RamseteController(Settings.kRamseteB, Settings.kRamseteZeta);
         mFeedforward = new SimpleMotorFeedforward(Settings.kS, Settings.kV, Settings.kA);
-        mRightController = new PIDController(3.0,0, -0.5);
-        mLeftController = new PIDController(3.0, 0, -0.5);
+        mRightController = new PIDController(3.0,0, 0.005);
+        mLeftController = new PIDController(3.0, 0, 0.005);
         mTimer = new Timer();
         mDriveKinematics = new DifferentialDriveKinematics(Settings.kTrackWidthMeters);
         SmartDashboard.putNumber("trajectory-seconds",-1);
