77686a4ffb7ee288d8f6604c4235b423e085445a
Agrim Sharma <75flyershd@gmail.com>
2022-03-29 09:50:31 -0400

    attempt to build trajectory manually

--- src/main/java/us/ilite/robot/TrajectoryCommandUtils.java
+++ src/main/java/us/ilite/robot/TrajectoryCommandUtils.java
@@ -7,19 +7,24 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.math.trajectory.TrajectoryConfig;
 import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrajectoryUtil;
+import edu.wpi.first.math.trajectory.constraint.CentripetalAccelerationConstraint;
+import edu.wpi.first.math.trajectory.constraint.DifferentialDriveKinematicsConstraint;
 import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import us.ilite.common.config.Settings;
+import us.ilite.robot.modules.NeoDriveModule;
 import us.ilite.robot.modules.VioletDriveModule;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -86,7 +91,7 @@ public class TrajectoryCommandUtils {
         Trajectory trajectory = new Trajectory();
         try {
             Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-            trajectory = new TrajectoryWrapper(TrajectoryUtil.fromPathweaverJson(trajectoryPath));
+            trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
         } catch (IOException ex) {
             System.out.println("Unable to open " + trajectoryJSON + " " + Arrays.toString(ex.getStackTrace()));
         }
@@ -103,6 +108,18 @@ public class TrajectoryCommandUtils {
             System.out.println("Unable to open " + trajectoryJSON + " " + Arrays.toString(ex.getStackTrace()));
         }
         return trajectory;
+    }
+
+    public static Trajectory buildExampleTrajectory() {
+        // TODO Normally this method would build the trajectory based off of the waypoints and config that is passed in
+        //  but I am going to keep it hard-coded for now
+        TrajectoryConfig config = new TrajectoryConfig(3 ,3);
+        config.addConstraint(new CentripetalAccelerationConstraint(3));
+        config.addConstraint(new DifferentialDriveKinematicsConstraint(new DifferentialDriveKinematics(NeoDriveModule.kTrackWidthFeet), 3));
+        ArrayList<Pose2d> waypoints = new ArrayList<Pose2d>();
+        waypoints.add(new Pose2d(1,1 ,new Rotation2d(30)));
+        waypoints.add(new Pose2d(new Translation2d(2, 2), new Rotation2d(30)));
+        return TrajectoryGenerator.generateTrajectory(waypoints, config);
     }
 
     /**
