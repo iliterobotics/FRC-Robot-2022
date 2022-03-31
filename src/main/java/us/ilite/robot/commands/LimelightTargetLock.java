bb26656c92085052d477cf59a0428ed899e85dcb
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2022-03-31 19:38:42 -0400

    Created LimelightTargetLock command

--- src/main/java/us/ilite/robot/commands/LimelightTargetLock.java
+++ src/main/java/us/ilite/robot/commands/LimelightTargetLock.java
@@ -2,29 +2,44 @@ package us.ilite.robot.commands;
 
 import com.flybotix.hfr.util.log.ILog;
 import com.flybotix.hfr.util.log.Logger;
+import edu.wpi.first.math.geometry.Rotation2d;
+import us.ilite.common.Data;
+import us.ilite.common.types.ELimelightData;
+import us.ilite.common.types.drive.EDriveData;
+import us.ilite.robot.Robot;
 import us.ilite.robot.modules.Limelight;
 
-@Deprecated
-public class LimelightTargetLock extends TargetLock {
+public class LimelightTargetLock {
 
     private ILog mLog = Logger.createLog(LimelightTargetLock.class);
 
-    private Limelight mLimelight;
+    private static Limelight mLimelight;
+    private static Rotation2d mTargetLockAngle;
+    private static final int kAllowableError = 5;
+    private static double mDistanceToTarget = 0;
 
     public LimelightTargetLock() {
-        super();
-        mLog.error("STARTED LIMELIGHT TARGET LOCK");
+
     }
 
-    @Override
+    private static Rotation2d calculateTheta() {
+        double tx = Robot.DATA.limelight.get(ELimelightData.TX);
+        mDistanceToTarget = Robot.DATA.limelight.get(ELimelightData.TARGET_RANGE_in)/12.0;
+        mTargetLockAngle = new Rotation2d(Math.atan(tx/mDistanceToTarget) + Robot.DATA.drivetrain.get(EDriveData.ACTUAL_HEADING_DEGREES));
+        return mTargetLockAngle;
+    }
+
     public void init(double pNow) {
         super.init(pNow);
-        mLog.error("STARTED LIMELIGHT TARGET LOCK");
+    }
+
+    public boolean update(double pNow) {
+        super.update(pNow);
+        return false;
     }
 
     public void shutdown(double pNow) {
         super.shutdown(pNow);
-        mLog.warn("SHUT DOWN LIMELIGHT TARGET LOCK");
     }
 
 }
