525cee7492dd2c3bd05caa77d0b1e7a597549be1
Agrim Sharma <75flyershd@gmail.com>
2022-04-07 16:13:22 -0400

    lower first turn count even further in auto

--- src/main/java/us/ilite/robot/controller/TwoBallController.java
+++ src/main/java/us/ilite/robot/controller/TwoBallController.java
@@ -15,7 +15,7 @@ import us.ilite.robot.commands.TurnToDegree;
 
 public class TwoBallController extends BaseAutonController {
     //This was -10 initially
-    private TurnToDegree mFirstTurn = new TurnToDegree(Rotation2d.fromDegrees(-8), 2);
+    private TurnToDegree mFirstTurn = new TurnToDegree(Rotation2d.fromDegrees(-6), 2);
     private boolean mFirstTurnComplete = false;
     private DriveStraight mFirstLeg = new DriveStraight(Distance.fromInches(50));
     private boolean mFirstLegComplete = false;
