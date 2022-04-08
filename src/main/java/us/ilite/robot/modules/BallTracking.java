ec34500e7d9d639ccf354d83d5d401f88599f484
Megan Farran <farranme23@pwcs-edu.org>
2022-04-08 17:10:30 -0400

    pixy debugging

--- src/main/java/us/ilite/robot/modules/BallTracking.java
+++ src/main/java/us/ilite/robot/modules/BallTracking.java
@@ -2,7 +2,6 @@ package us.ilite.robot.modules;
 
 import io.github.pseudoresonance.pixy2api.*;
 import io.github.pseudoresonance.pixy2api.links.SPILink;
-import io.github.pseudoresonance.pixy2api.links.Link;
 import io.github.pseudoresonance.pixy2api.Pixy2CCC.*;
 import us.ilite.common.types.EPixyData;
 
@@ -10,37 +9,61 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class BallTracking extends Module {
-    Link link = new SPILink();
-    Pixy2 pixy = Pixy2.createInstance(link);
-    Block largestBlock = null;
-    Block secondLargestBlock = null;
+    private Pixy2 pixy;
+    private int state = -1 ;
+    boolean isCamera = false;
 
     public BallTracking() {
-        pixy.init();
-        System.out.println("pixy ccc " + pixy.getCCC());
-        largestBlock = findLargestBLock();
-        secondLargestBlock = findSecondLargestBLock();
+//        pixy.init();
+        pixy = Pixy2.createInstance(Pixy2.LinkType.SPI);
     }
 
     public void readInputs() {
-        db.pixydata.set(EPixyData.LARGEST_XCoorinate, largestBlock.getX());
-        db.pixydata.set(EPixyData.LARGEST_YCoordniate, largestBlock.getY());
-        db.pixydata.set(EPixyData.LARGEST_WIDTH, largestBlock.getWidth());
-        db.pixydata.set(EPixyData.LARGEST_HEIGHT, largestBlock.getHeight());
-        db.pixydata.set(EPixyData.LARGEST_SIGNATURE, largestBlock.getSignature());
-        db.pixydata.set(EPixyData.LARGEST_ANGLE_FROM_CAMERA, largestBlock.getAngle());
 
-        db.pixydata.set(EPixyData.SECOND_LARGEST_XCoorinate, secondLargestBlock.getX());
-        db.pixydata.set(EPixyData.SECOND_LARGEST_YCoordniate, secondLargestBlock.getY());
-        db.pixydata.set(EPixyData.SECOND_LARGEST_WIDTH, secondLargestBlock.getWidth());
-        db.pixydata.set(EPixyData.SECOND_LARGEST_HEIGHT, secondLargestBlock.getHeight());
-        db.pixydata.set(EPixyData.SECOND_LARGEST_SIGNATURE, secondLargestBlock.getSignature());
-        db.pixydata.set(EPixyData.SECOND_LARGEST_ANGLE_FROM_CAMERA, secondLargestBlock.getAngle());
+        if(!isCamera) {
+            state = pixy.init(0);
+            isCamera = state >=0;
+            System.out.println("state: " + state);
+        }
+        if (!isCamera) {
+            return;
+        }
+
+        Block largestBlock = findLargestBLock();
+        Block secondLargestBlock = findSecondLargestBLock();
+
+        if (largestBlock != null) {
+            db.pixydata.set(EPixyData.LARGEST_XCoorinate, largestBlock.getX());
+            db.pixydata.set(EPixyData.LARGEST_YCoordniate, largestBlock.getY());
+            db.pixydata.set(EPixyData.LARGEST_WIDTH, largestBlock.getWidth());
+            db.pixydata.set(EPixyData.LARGEST_HEIGHT, largestBlock.getHeight());
+            db.pixydata.set(EPixyData.LARGEST_SIGNATURE, largestBlock.getSignature());
+            db.pixydata.set(EPixyData.LARGEST_ANGLE_FROM_CAMERA, largestBlock.getAngle());
+        } else {
+            System.out.println("largest block is null");
+        }
+
+        if (secondLargestBlock != null) {
+            db.pixydata.set(EPixyData.SECOND_LARGEST_XCoorinate, secondLargestBlock.getX());
+            db.pixydata.set(EPixyData.SECOND_LARGEST_YCoordniate, secondLargestBlock.getY());
+            db.pixydata.set(EPixyData.SECOND_LARGEST_WIDTH, secondLargestBlock.getWidth());
+            db.pixydata.set(EPixyData.SECOND_LARGEST_HEIGHT, secondLargestBlock.getHeight());
+            db.pixydata.set(EPixyData.SECOND_LARGEST_SIGNATURE, secondLargestBlock.getSignature());
+            db.pixydata.set(EPixyData.SECOND_LARGEST_ANGLE_FROM_CAMERA, secondLargestBlock.getAngle());
+        } else {
+            System.out.println("second largest block is null");
+        }
+    }
+
+    public void setOutputs() {
+        pixy.setCameraBrightness(255);
     }
 
 
     public Block findLargestBLock() {
-        pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 50);
+        Pixy2CCC pixyccc = pixy.getCCC();
+        pixyccc.getBlocks(true, Pixy2CCC.CCC_SIG2, 25);
+//        System.out.println("found " + blockCount + " blocks");
         final ArrayList<Block> blocks = pixy.getCCC().getBlockCache();
         blocks.sort(new Sorter());
         if (blocks.size() > 0) {
@@ -51,7 +74,7 @@ public class BallTracking extends Module {
     }
 
     public Block findSecondLargestBLock() {
-        pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 50);
+        pixy.getCCC().getBlocks(true, Pixy2CCC.CCC_SIG2, 25);
         final ArrayList<Block> blocks = pixy.getCCC().getBlockCache();
         blocks.sort(new Sorter());
         if (blocks.size() > 1) {
