622306c146b15715b2a897ca102cb063eb0d9672
Megan Farran <farranme23@pwcs-edu.org>
2022-04-16 13:07:27 -0400

    Switch auton controller tested

--- src/main/java/us/ilite/robot/controller/TexasSwitchController.java
+++ src/main/java/us/ilite/robot/controller/TexasSwitchController.java
@@ -3,31 +3,35 @@ package us.ilite.robot.controller;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.Timer;
+import us.ilite.common.Distance;
 import us.ilite.common.types.EIntakeData;
 import us.ilite.common.types.drive.EDriveData;
 import us.ilite.robot.Enums;
+import us.ilite.robot.commands.DriveStraight;
 import us.ilite.robot.commands.TurnToDegree;
 
 public class TexasSwitchController extends BaseAutonController {
     private Timer mTimer;
-    private TurnToDegree mFirstTurn = new TurnToDegree(Rotation2d.fromDegrees(90), 2d);
+    private TurnToDegree mFirstTurn = new TurnToDegree(Rotation2d.fromDegrees(-45), 2d);
+    private DriveStraight mTaxi = new DriveStraight(Distance.fromFeet(4.5));
     public void initialize() {
         mTimer = new Timer();
         mTimer.reset();
         mTimer.start();
     }
     public void updateImpl() {
-        if (mTimer.get() < 0.5) {
-            setIntakeArmEnabled(true);
-        } else if (mTimer.get() < 1.0) {
-            db.drivetrain.set(EDriveData.STATE, Enums.EDriveState.RESET);
-            mFirstTurn.init(mTimer.get());
-        }
-        else if (mTimer.get() < 3.0) {
-            db.intake.set(EIntakeData.ROLLER_STATE, Enums.ERollerState.PERCENT_OUTPUT);
+        if (mTimer.get() < 1.0) {
             reverseCargo();
-        } else {
-
+            setIntakeArmEnabled(true);
+        } else if (mTimer.get() < 1.1) {
+            mFirstTurn.init(mTimer.get());
+        } else if (mTimer.get() < 3.0) {
+            setIntakeArmEnabled(false);
+            mFirstTurn.update(mTimer.get());
+        } else if (mTimer.get() < 3.1) {
+            mTaxi.init(mTimer.get());
+        } else if (mTimer.get() < 6.0) {
+            mTaxi.update(mTimer.get());
         }
     }
 }
